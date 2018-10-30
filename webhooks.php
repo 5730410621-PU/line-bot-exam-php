91f6a0626401c9f43f4bb3edef9a408b946c574f
peerakit umbangtalad <>
2018-10-30 12:04:03 +0700

    no message

--- webhooks.php
+++ webhooks.php
@@ -21,31 +21,21 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
-			// $messages = [
-			// 	'type' => 'text',
-			// 	'text' => $text
-			// ];
-
 			$messages = [
-				array(
-					'type' => 'text',
-					'text' => "Hello"
-				),
-				array(
-					'type' => 'text',
-					'text' => "feed"
-				),
-				array(
-					'type' => 'text',
-					'text' => $text
-				)
+				'type' => 'text',
+				'text' => $text
 			];
+
+			$messages1 = [
+				'type' => 'text',
+				'text' => 'love u'
+			];			
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => [$messages],
+				'messages' => [$messages,$messages1],
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
