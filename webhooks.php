caed16ff960cdaf00e1fd7d33623bb9f6e6fee21
peerakit umbangtalad <>
2018-10-31 10:39:53 +0700

    no message

--- webhooks.php
+++ webhooks.php
@@ -22,8 +22,9 @@ $content = file_get_contents('php://input');
       $arrayPostData['messages'][1]['type'] = "sticker";
       $arrayPostData['messages'][1]['packageId'] = "2";
       $arrayPostData['messages'][1]['stickerId'] = "34";
-      pushMsg($arrayHeader,$arrayPostData);
+      ReplyMsg($arrayHeader,$arrayPostData);
    }
+
    function pushMsg($arrayHeader,$arrayPostData){
       $strUrl = "https://api.line.me/v2/bot/message/push";
       $ch = curl_init();
@@ -37,4 +38,18 @@ $content = file_get_contents('php://input');
       $result = curl_exec($ch);
       curl_close ($ch);
    }
+
+   function replyMsg($arrayHeader,$arrayPostData){
+	$strUrl = "https://api.line.me/v2/bot/message/reply";
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL,$strUrl);
+	curl_setopt($ch, CURLOPT_HEADER, false);
+	curl_setopt($ch, CURLOPT_POST, true);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);    
+	curl_setopt($ch, CURLOPT_POSTFIELDS,json_encode($arrayPostData));
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	$result = curl_exec($ch);
+	curl_close ($ch);
+}
    exit;echo "OK";
