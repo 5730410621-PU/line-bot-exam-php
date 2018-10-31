b9985ad20436adc491081c1975dac9fe0fcfd5bc
peerakit umbangtalad <>
2018-10-31 23:37:47 +0700

    no message

--- webhooks.php
+++ webhooks.php
@@ -142,7 +142,7 @@ function getRichMenu($header){
 	$strUrl = "https://api.line.me/v2/bot/richmenu/list";
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_URL,$strUrl);
-	curl_setopt($ch, CURLOPT_HEADER, false);
+	curl_setopt($ch, CURLOPT_HEADER, true);
 	curl_setopt($ch, CURLOPT_POST, false);
 	curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);
 	curl_setopt($ch, CURLOPT_POSTFIELDS, null);
