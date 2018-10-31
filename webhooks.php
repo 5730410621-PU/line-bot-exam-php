328ff60bb332ca6c9b662c817fa5cbbaeab27e52
peerakit umbangtalad <>
2018-10-31 23:44:56 +0700

    no message

--- webhooks.php
+++ webhooks.php
@@ -150,7 +150,12 @@ function getRichMenu($header){
 	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 
 	$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-	$result = curl_exec($ch);
+
+	if (curl_errno($ch)) { 
+		$result = curl_error($ch); 
+	 } else{
+		$result = curl_exec($ch);
+	 }
 	curl_close ($ch);
 
 	$richMenuList = (array) json_decode($result,true);
