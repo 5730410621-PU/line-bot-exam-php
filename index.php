1ce1f7a96199768d9f00f6f74a16a695842bee82
peerakit umbangtalad <>
2018-10-31 14:44:23 +0700

    no message

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 $str = "{'richMenuId': 'richmenu-88c05ef6921ae53f8b58a25f3a65faf7'}";
-
-echo json_decode($str['richMenuId']);
+$strJson =json_decode($str);
+echo $strJson['richMenuId'];
 //echo json_encode($str);
