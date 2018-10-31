a02036e08be2bef8028d8807f5fc7a2acaf26ede
peerakit umbangtalad <>
2018-10-31 22:49:33 +0700

    no message

--- webhooks.php
+++ webhooks.php
@@ -143,6 +143,8 @@ function getRichMenu($header){
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_URL,$strUrl);
 	curl_setopt($ch, CURLOPT_POST, false);
+	curl_setopt($ch, CURLOPT_HEADER  , true);  
+	curl_setopt($ch, CURLOPT_NOBODY  , true);
 	curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
 
